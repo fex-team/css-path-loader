e8e5eaec39657971c27da6c438e4ac178da1a856
dongtiancheng <dongtiangche@gmail.com>
2015-12-09 19:11:20 +0800

    bug fixed

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 module.exports = function (source, map) {
     this.cacheable && this.cacheable()
-    var namespace = this.resourcePath.replace(/[\W\w]+(?=src\b)/, '').replace(/\.(scss|less)/, '')
+
+    // 找到入口文件绝对位置
+    var entryRelativePath = ''
+    if (Object.prototype.toString.call(this.options.entry) === '[object Array]') {
+        for (var value of this.options.entry) {
+            if (value.indexOf('webpack') === -1) {
+                entryRelativePath = value
+            }
+        }
+    } else {
+        entryRelativePath = this.options.entry
+    }
+
+    // 得到了入口文件的绝对位置
+    var entryAbsolutePath = this.options.context + entryRelativePath.replace(/^\./g, '')
+
+    // 得到入口文件文件夹路径
+    var entryAbsoluteFolderPathArray = entryAbsolutePath.split('/')
+    entryAbsoluteFolderPathArray.pop()
+
+    var namespace = this.resourcePath.replace(entryAbsoluteFolderPathArray.join('/') + '/', '').replace(/\.(less|scss)/, '')
 
     var nameArray = namespace.split('/')
     nameArray.pop()
-    namespace = nameArray.join('-')
+    var nameStr = nameArray.join('-')
 
-    // 豁免全局样式
-    if (namespace !== 'src') {
-        source = '.' + namespace + '{' + source + '}'
+    if (nameStr) {
+        source = '.' + nameStr + '{' + source + '}'
     }
 
     this.callback(null, source, map)
